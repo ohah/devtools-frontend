2bb9600017dd89a1ee1862dc162ae7fa57516660
ohah <bookyoon173@gmail.com>
2026-01-07 01:15:20 +0900

    feat: redux-frontend-extension-add

--- front_end/entrypoints/devtools_app/devtools_app.ts
+++ front_end/entrypoints/devtools_app/devtools_app.ts
@@ -29,6 +29,7 @@ import '../../panels/recorder/recorder-meta.js';
 import '../../panels/whats_new/whats_new-meta.js';
 import '../../panels/welcome/welcome-meta.js';
 import '../../panels/session_replay/session_replay-meta.js';
+import '../../panels/redux/redux-meta.js';
 
 import * as Root from '../../core/root/root.js';
 import * as Main from '../main/main.js';
