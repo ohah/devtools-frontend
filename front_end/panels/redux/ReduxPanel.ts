e20328549128bee3708066556c143812e00f7167
ohah <bookyoon173@gmail.com>
2026-01-11 16:11:55 +0900

    fix: redux-panel-ui restore

--- front_end/panels/redux/ReduxPanel.ts
+++ front_end/panels/redux/ReduxPanel.ts
@@ -9,15 +9,6 @@ import * as UI from '../../ui/legacy/legacy.js';
 
 import { getReduxExtensionBridge, initializeReduxBridge, type ReduxExtensionBridge } from './ReduxExtensionBridge.js';
 
-export const DEFAULT_VIEW = (input, _output, target) => {
-  render(html`
-    <div>
-      <iframe class="redux-devtools-iframe" src=${reduxDevToolsPage}
-          style="width:100%; height:100%; border:none"></iframe>
-    </div>`,
-    target);
-};
-
 export class ReduxPanel extends UI.Panel.Panel {
   #iframe: HTMLIFrameElement | null = null;
   #bridge: ReduxExtensionBridge;
@@ -34,6 +25,10 @@ export class ReduxPanel extends UI.Panel.Panel {
 
     // Create iframe directly / iframe 직접 생성
     this.#iframe = document.createElement('iframe');
+    this.#iframe.className = 'redux-devtools-iframe';
+    this.#iframe.style.width = '100%';
+    this.#iframe.style.height = '100%';
+    this.#iframe.style.border = 'none';
 
     // Build URL for plugin HTML / 플러그인 HTML URL 구성
     // Use the same base URL as the current page / 현재 페이지와 동일한 base URL 사용
@@ -49,6 +44,9 @@ export class ReduxPanel extends UI.Panel.Panel {
       reduxDevToolsPage = `${basePath}/panels/plugins/redux-plugin/index.html` as Platform.DevToolsPath.UrlString;
     }
 
+    // Set iframe source / iframe 소스 설정
+    this.#iframe.src = reduxDevToolsPage;
+
     this.#iframe.onload = () => {
       if (this.#iframe?.contentWindow) {
         // Wait a bit for iframe content to fully load / iframe 콘텐츠가 완전히 로드될 때까지 약간 대기
@@ -60,11 +58,13 @@ export class ReduxPanel extends UI.Panel.Panel {
         }, 100);
       }
     };
+
+    // Append iframe to content element / iframe을 content element에 추가
+    this.contentElement.appendChild(this.#iframe);
   }
 
   override wasShown(): void {
     super.wasShown();
-
     // When panel is shown, flush buffered messages again for new connections / 패널이 표시될 때 새 연결을 위해 버퍼된 메시지를 다시 플러싱
     // This ensures history is sent when panel is reopened / 패널이 다시 열릴 때 히스토리가 전송되도록 보장
     if (this.#iframe?.contentWindow) {
@@ -87,4 +87,3 @@ export class ReduxPanel extends UI.Panel.Panel {
     // Don't cleanup - bridge is global and keeps listening / 정리하지 않음 - bridge는 전역이고 계속 리스닝
   }
 }
-
